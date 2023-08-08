f2baf8e9edafb434f21b57e12f5e57c7cb415adc
paulAlexSerban <paul.alex.serban@gmail.com>
2023-08-08 15:05:56 +0300

    feat: git issues

--- frontend/landing-ssg/src/frontend/js/pages/index.js
+++ frontend/landing-ssg/src/frontend/js/pages/index.js
@@ -5,7 +5,7 @@ const second = 1000,
     day = hour * 24;
 
 // set the date we are counting down to
-let countDown = new Date("Aug 05, 2023 23:59:59").getTime();
+let countDown = new Date("Aug 05, 2024 23:59:59").getTime();
 
 // update the countdown every 1 second
 
